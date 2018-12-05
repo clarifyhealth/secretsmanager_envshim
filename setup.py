800d9ba06701d36f0a8794c4850441baaa67ceef
warrenronsiek <warrenronsiek@gmail.com>
2018-12-05 09:27:36 -0800

    updated gitignore, setup.py

--- setup.py
+++ setup.py
@@ -3,12 +3,10 @@ from setuptools import setup
 setup(name='secretsmanager_envshim',
       version='0.1',
       description='An environment variable based shim around AWS secrets manager',
-      url='http://github.com/storborg/funniest',
-      author='Flying Circus',
-      author_email='flyingcircus@example.com',
+      url='https://github.com/clarifyhealth/secretsmanager_envshim',
+      author='Warren Ronsiek',
+      author_email='warren@clarifyhealth.com',
       license='MIT',
-      packages=['funniest'],
-      install_requires=[
-          'markdown',
-      ],
+      packages=['secretsmanager_envshim'],
+      install_requires=['boto3'],
       zip_safe=False)
